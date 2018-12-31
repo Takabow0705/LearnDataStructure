b6c9029c228184780d1a546d379ccd5cd27ec4e4
haraguchi_takahiro <hrgc.takahiro@outlook.jp>
2018-12-31 22:24:40 +0900

    [REVISE] revise some typos

--- Golang/chapter3/ArrayQueue/utils/testUtils.go
+++ Golang/chapter3/ArrayQueue/utils/testUtils.go
@@ -2,13 +2,12 @@ package utils
 
 import(
 	"fmt"
-	"testing"
 )
 
 func main(){
 	test1 := Compare(0,1)
-	test2 := Compae(-129,324)
-	test3 := Compae(3,3)
+	test2 := Compare(-129,324)
+	test3 := Compare(3,3)
 
 	fmt.Printf("Expect: 1 Answer: %v, Expect: 324 Answer: %v, Expect: 3 Answer: %v",test1,test2,test3)
 	
