7fd3fd878d995db6b1596773b88731db678d5dac
haraguchi_takahiro <hrgc.takahiro@outlook.jp>
2018-12-31 22:23:14 +0900

    [ADD] add stack and queue in main function

--- Golang/chapter3/main.go
+++ Golang/chapter3/main.go
@@ -2,8 +2,24 @@ package main
 
 import(
 	"fmt"
+	"./ArrayStack"
+	"./ArrayQueue"
 )
 
 func main(){
+	stack := array_stack.NewArrayStack()
+	queue := array_queue.NewArrayQueue()
+
+	fmt.Println(stack.Size())
+	stack.Add(0,3)
+	stack.Add(1,45)
+	stack.Add(2,22)
+	fmt.Println(stack.Size())
+
+	fmt.Println(queue.Size())
+	queue.Add(0,3)
+	queue.Add(1,45)
+	queue.Add(2,22)
+	fmt.Println(queue.Size())	
 	
 }
