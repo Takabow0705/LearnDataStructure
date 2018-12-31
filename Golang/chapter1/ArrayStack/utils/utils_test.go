cabf6658934e241873c859b7b335c24126f1791b
haraguchi_takahiro <hrgc.takahiro@outlook.jp>
2018-12-31 19:22:48 +0900

    [CREATE] create util func to manipulate arrays

---
+++ Golang/chapter1/ArrayStack/utils/utils_test.go
@@ -0,0 +1,15 @@
+package utils
+
+import(
+	"fmt"
+)
+
+func main(){
+	test1 := Compare(0,1)
+	test2 := Compae(-129,324)
+	test3 := Compae(3,3)
+
+	fmt.Println("Expect: 1 Answer: %v, Expect: 324 Answer: %v, Expect: 3 Answer: %v"
+				,test1,test2,test3)
+	
+}
