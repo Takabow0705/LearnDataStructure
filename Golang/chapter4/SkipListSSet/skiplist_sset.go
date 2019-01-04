b6dbc60912416fe70b8513303562fd70acdc5819
haraguchi_takahiro <hrgc.takahiro@outlook.jp>
2019-01-04 23:21:42 +0900

    [add] add pickHeight() method

--- Golang/chapter4/SkipListSSet/skiplist_sset.go
+++ Golang/chapter4/SkipListSSet/skiplist_sset.go
@@ -7,5 +7,16 @@ import(
 type node struct{
 	x string
 	height int
-	node *[]next
+	node []*next
+}
+
+func pickHeight() int{
+	r := rand.Int()
+	height := 0
+
+	for r&1 != 0{
+		height++
+		r >>= 1
+	}
+	return height
 }
