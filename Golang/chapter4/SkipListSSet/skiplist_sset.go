85cb15b31069dbdcaaec8fc52de329dc6891c25f
haraguchi_takahiro <hrgc.takahiro@outlook.jp>
2019-01-05 21:37:53 +0900

    [add] create newNode() method

--- Golang/chapter4/SkipListSSet/skiplist_sset.go
+++ Golang/chapter4/SkipListSSet/skiplist_sset.go
@@ -7,9 +7,11 @@ import(
 type node struct{
 	x string
 	height int
-	node []*next
+	next []*node
 }
 
+//新しいnodeの高さを乱数で設定します。
+//rの二進数表記で1の数をカウントする。
 func pickHeight() int{
 	r := rand.Int()
 	height := 0
@@ -19,4 +21,12 @@ func pickHeight() int{
 		r >>= 1
 	}
 	return height
+}
+
+func newNode(x string,h int) *node{
+	return &node{
+		x : x,
+		height : h,
+		next : make([]*node,h + 1),
+	}
 }
