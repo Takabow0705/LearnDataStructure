ad2457307c9f473276e829d83da22a8b34c13315
haraguchi_takahiro <hrgc.takahiro@outlook.jp>
2019-01-08 22:30:08 +0900

    [complete] complete execution program

--- Golang/chapter4/SkipListSSet/execute_skiplist_sset.go
+++ Golang/chapter4/SkipListSSet/execute_skiplist_sset.go
@@ -8,6 +8,12 @@ import (
 func main(){
 
 	list := skiplist_sset.NewSkipListSSet() 
-	fmt.Println(lis.Size())
+	fmt.Println(list.Size())
+	list.Add(5)
+	list.Add(3)
+	list.Add(12)
+	fmt.Println(list.Size())
+	list.Remove(1)
+	fmt.Println(list.Size())
 
 }
