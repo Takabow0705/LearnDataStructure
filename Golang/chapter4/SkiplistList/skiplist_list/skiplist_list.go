8ffe23e536e15ccc1653425718bc7a254a134a69
haraguchi_takahiro <hrgc.takahiro@outlook.jp>
2019-01-09 22:34:10 +0900

    [add] add node constructor

--- Golang/chapter4/SkiplistList/skiplist_list/skiplist_list.go
+++ Golang/chapter4/SkiplistList/skiplist_list/skiplist_list.go
@@ -1,5 +1,36 @@
 package skiplist_list
 
 import(
-	""
+	"math/rand"
+	"strconv"
+	"../../../utils"
 )
+
+type node struct{
+	x int
+	length int
+	height int
+	nexts []*node
+}
+
+//新しいnodeの高さを乱数で設定します。
+//rの二進数表記で1の数をカウントする。
+func pickHeight() int{
+	r := rand.Int()
+	height := 0
+
+	for r&1 != 0{
+		height++
+		r >>= 1
+	}
+	return height
+}
+
+func newNode(x int,h int,len int) *node{
+	return &node{
+		x : x,
+		length : len,
+		height : h,
+		nexts : make([]*node,h + 1),
+	}
+}
