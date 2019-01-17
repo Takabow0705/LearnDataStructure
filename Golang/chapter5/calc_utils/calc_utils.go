7d10bcf7522fe2fd4a62f76a34e5595f032058b4
haraguchi_takahiro <hrgc.takahiro@outlook.jp>
2019-01-17 21:40:20 +0900

    [add] add stringTouint64() func

--- Golang/chapter5/calc_utils/calc_utils.go
+++ Golang/chapter5/calc_utils/calc_utils.go
@@ -1,4 +1,25 @@
 package calc_utils
 
-type func() int HashCalcStrategy
+import(
+	"math/rand"
+	"strconv"
+)
+type func(x string,d uint64) uint64 HashCalcStrategy
+
+//乗算ハッシュ法を実装します。 
+//d : 次数
+//x : 要素
+//具体的には以下の計算を実装する。
+// hash(x,d) = (z * uint64(x))
+func MultipleHash(x string,d uint64) uint64{
+	return (rand.Uint64*Uint64(x)) >> (64 - d)
+}
+
+func stringTouint64(x string) uint64{
+	bytes := []byte(x)
+	result := binary.LittleEndian.Uint16(bytes)
+	return uint64(result)
+}
+
+
 
