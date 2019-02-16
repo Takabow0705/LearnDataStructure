0e9cb929896562e1b95bbf803ab96fe4dbf8e640
haraguchi_takahiro <hrgc.takahiro@outlook.jp>
2019-02-17 00:14:57 +0900

    [create] add interfaces to solve issue #1

---
+++ Golang/interfaces/interfaces.go
@@ -0,0 +1,20 @@
+type Queue interface{
+	Add(x interface{})
+	Remove()
+}
+
+type List interface{
+	Size()
+	Get(i int)
+	Set(i int,x interface{})
+	Add(i int,x interface{})
+	Remove(i int)
+}
+
+type Set interface{
+	Size()
+	Add()
+	Remove()
+	Find()
+}
+
