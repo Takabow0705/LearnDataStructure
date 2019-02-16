0363409fdd1c2c5ace541f4e0538b1404b5d7243
haraguchi_takahiro <hrgc.takahiro@outlook.jp>
2019-02-16 20:53:29 +0900

    [add] add FinadEQ method

--- Golang/chapter6/BinarySearchTree/binary_search_tree/binary_search_tree.go
+++ Golang/chapter6/BinarySearchTree/binary_search_tree/binary_search_tree.go
@@ -17,8 +17,31 @@ func newNode() *node {
 
 type BinarySearchTree struct {
 	r *node
+	n int
 }
 
 func NewBT() *BinarySearchTree {
 	return &BinarySearchTree{}
+}
+
+//二分探索木の探索を行い、探索結果を返します。
+//存在するならデータを、しないならnilを返します。
+func(bst *BinarySearchTree) FindEQ(x int) interface{}{
+	w := bst.r
+
+	for w != nil{
+		comp := utils.compare(x,w.x)
+
+		if comp < 0{
+			w = w.left
+		}
+		if comp > 0{
+			w = w.right
+		}
+
+		if comp == 0{
+			return w.x
+		}
+		return nil
+	}
 }
