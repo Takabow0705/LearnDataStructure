1506ffc8b1572c873c8adb0dd00e3edb93c51c7a
haraguchi_takahiro <hrgc.takahiro@outlook.jp>
2019-03-04 23:04:58 +0900

    [resolve] add String() method  to resolve #5

--- Golang/chapter2/ArrayDeque/array_deque/array_deque.go
+++ Golang/chapter2/ArrayDeque/array_deque/array_deque.go
@@ -1,12 +1,13 @@
 package array_deque
 
 import (
+	"fmt"
 	"../../../utils"
 )
 
 type ArrayDeque struct{
 	n,i,cap int
-	buf []utils.T
+	buf []string
 }
 
 //ArrayDeque構造体を作成します。
@@ -18,7 +19,7 @@ func NewArrayDeque() ArrayDeque{
 //配列の指定位置に要素を追加します
 //また、容量が不足している場合は
 //resize()を呼び出して、配列の大きさを増やします。
-func (ad *ArrayDeque) Add(i int ,v utils.T){
+func (ad *ArrayDeque) Add(i int ,v string){
 
 	if ad.is_full(){
 		ad.resize()
@@ -40,7 +41,7 @@ func (ad *ArrayDeque) Add(i int ,v utils.T){
 
 //i番目以降の要素を全てひとつ後ろにずらします。
 //これにより、i番目に新しい要素を入れることが可能となります。
-func (ad *ArrayDeque) Remove(i int)utils.T{
+func (ad *ArrayDeque) Remove(i int)string{
 
 	ret := ad.buf[(i + ad.i)%ad.cap]
 	if i < ad.n/2{
@@ -68,12 +69,12 @@ func (ad *ArrayDeque) Size() int{
 }
 
 //削除対象の要素を返します。
-func (ad *ArrayDeque) Get(i int) utils.T{
+func (ad *ArrayDeque) Get(i int) string{
 	return ad.buf[(ad.i + i)%ad.cap]
 }
 
 //i番目の要素をvで入れ替え、以前の要素を返す。
-func (ad *ArrayDeque) Set(i int,v utils.T) utils.T{
+func (ad *ArrayDeque) Set(i int,v string) string{
 	ret := ad.buf[(ad.i + i)%ad.cap]
 
 	ad.buf[(ad.i + i)%ad.cap] = v
@@ -90,7 +91,7 @@ func (ad *ArrayDeque) is_full() bool{
 //新しい配列をレシーバの構造体の配列として再設定します。
 func (ad *ArrayDeque) resize(){
 	cap_new := utils.Max(2 * ad.n,1) 
-	buf_new := make([]utils.T,cap_new)
+	buf_new := make([]string,cap_new)
 
 	for i:=0; i < ad.n; i++{
 		buf_new[i] = ad.buf[(i+ad.i)%ad.cap]
@@ -107,11 +108,23 @@ func (ad *ArrayDeque) is_sparse() bool{
 }
 
 //削除対象インデックスの値に応じてインデックスを再設定する
-//Add(i int,x utils.T)の補助メソッド
+//Add(i int,x string)の補助メソッド
 func (ad *ArrayDeque) select_index() int{
 	if ad.i == 0{
 		return ad.cap - 1
 	}else{
 		return ad.i - 1
 	}
+}
+
+//データ構造を文字列として出力します。
+func (ad *ArrayDeque) String() string{
+	result := ""
+	size := ad.Size()
+
+	for i:=0; i < size; i++{
+		str := fmt.Sprintf("{%d:%s},\n",i,ad.Get(i))
+		result += str
+	}
+	return result
 }
--- Golang/chapter2/ArrayDeque/execute_array_deque.go
+++ Golang/chapter2/ArrayDeque/execute_array_deque.go
@@ -3,19 +3,19 @@ package main
 import(
 	"fmt"
 	"./array_deque"
-	"../../utils"
 )
 
 func main(){
 	deque := array_deque.NewArrayDeque()
 
 	fmt.Println(deque.Size())
-	deque.Add(0,utils.T(3))
-	deque.Add(3,utils.T(45))
-	deque.Add(5,utils.T(232))
+	deque.Add(0,string(3))
+	deque.Add(3,string(45))
+	deque.Add(5,string(232))
 	fmt.Println(deque.Size())
-	deque.Add(3,utils.T(45))
-	deque.Add(2,utils.T(232))
+	deque.Add(3,string(45))
+	deque.Add(2,string(232))
 	fmt.Println(deque.Size())
+	fmt.Println(deque.String())
 	
 }
